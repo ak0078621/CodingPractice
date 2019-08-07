6a8633a29a7593fe01db65b77ec703849cbc1814
ak0078621 <ak0078621@gmail.com>
2019-08-06 21:13:00 -0400

    Update FizzBuzzType.java

--- FizzBuzz/src/pkg1/FizzBuzzType.java
+++ FizzBuzz/src/pkg1/FizzBuzzType.java
@@ -1,10 +1,30 @@
 package pkg1;
 
 public class FizzBuzzType {
-
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FizzBuzzType fizz = new FizzBuzzType();
+		fizz.printTheNumbers();
 
+	}
+
+	private void printTheNumbers() {
+		
+		int fizz = 3;
+		int buzz = 5;
+		int fizzbuzz = 3*5;
+		for(int i=1;i<=100;i++){
+			if((i%fizzbuzz)==0){
+				System.out.println("FizzBuzz");
+			}else if(i%buzz==0){
+				System.out.println("Buzz");
+			}else if(i%fizz==0){
+				System.out.println("Fizz");
+			}else{
+				System.out.println(i);
+			}
+		}
+		
 	}
 
 }
